e922e36caf22b129aa433c4e3b3a774cd0ea10c1
Alex J <alexjovermorales@gmail.com>
2020-07-08 20:31:27 +0200

    Initial project setup + first article card

---
+++ components/ArticleCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <nuxt-link :to="`/${slug}`">{{ title }}</nuxt-link>
+    </header>
+    <p>{{ description }}</p>
+    <footer>
+      <img :src="author.image" :alt="author.name" />
+      <span>{{ author.name }}</span>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    slug: String,
+    description: String,
+    author: Object,
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Articles</h1>
+    <div>
+      <ArticleCard
+        v-for="article in articles"
+        :key="article.title"
+        :title="article.title"
+        :description="article.description"
+        :author="article.author"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    const articles = [
+      {
+        title: 'How to make your articles Vuetiful',
+        description:
+          'This article guides you through all the steps to make an article shine with your favourite framework, Vue',
+        author: {
+          name: 'Naruto Uzumaki',
+          image: 'https://wallpaperaccess.com/full/2866246.jpg',
+        },
+      },
+    ]
+
+    return { articles }
+  },
+}
+</script>
